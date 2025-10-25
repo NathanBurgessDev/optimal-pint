82dafdfbad94c686041029a9eb5120fe7d34e831
James Harvey <44349936+jmshrv@users.noreply.github.com>
2025-10-26 00:30:39 +0100

    Scraper!

--- src/internal/fetcher/fetcher.go
+++ src/internal/fetcher/fetcher.go
@@ -2,7 +2,11 @@ package fetcher
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -17,6 +21,148 @@ func New(db *sqlx.DB) *Fetcher {
 		client: &http.Client{},
 		db:     db,
 	}
+}
+
+func (f *Fetcher) UpdateMenu(venueID int, salesAreaID int, menuID int) error {
+	url := fmt.Sprintf("https://ca.jdw-apps.net/api/v0.1/jdw/venues/%d/sales-areas/%d/menus/%d", venueID, salesAreaID, menuID)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", "Bearer 1|SFS9MMnn5deflq0BMcUTSijwSMBB4mc7NSG2rOhqb2765466")
+
+	res, err := f.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	var menu MenuDetailResponse
+
+	if err := json.NewDecoder(res.Body).Decode(&menu); err != nil {
+		return err
+	}
+
+	for _, category := range menu.Data.Categories {
+		if category.Name == "Includes a drink" {
+			continue
+		}
+
+		for _, itemGroup := range category.ItemGroups {
+			for _, item := range itemGroup.Items {
+				var units float64
+
+				pattern := `([\d.]+)\s*units`
+				re := regexp.MustCompile(pattern)
+
+				matches := re.FindStringSubmatch(item.Description)
+				if len(matches) >= 2 {
+					units, _ = strconv.ParseFloat(matches[1], 64)
+				}
+
+				if len(item.Options.Portion.Options) == 0 {
+					continue
+				}
+
+				maxOption := item.Options.Portion.Options[0]
+
+				for _, option := range item.Options.Portion.Options {
+					if option.Value.Price.Value > maxOption.Value.Price.Value {
+						maxOption = option
+					}
+				}
+
+				realPrice := maxOption.Value.Price.Value
+				hasDeal := false
+
+				if len(item.Options.Linked) != 0 {
+					amount := 0
+					dealPrice := 0.0
+
+					link := item.Options.Linked[0]
+
+					fmt.Sscanf(link.Name, "Any %d for £%f", &amount, &dealPrice)
+
+					if amount != 0 && dealPrice != 0 {
+						hasDeal = true
+						realPrice = dealPrice / float64(amount)
+					}
+				}
+
+				optimality := realPrice / units
+
+				_, err := f.db.Exec(
+					"INSERT into Drinks (DrinkName, PubId, Units, Price, Amount, Category, Optimality, HasDeal) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+					item.Name, venueID, units, realPrice, 67, category.Name, optimality, hasDeal,
+				)
+
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (f *Fetcher) UpdateBars(venueID int, salesArea SalesArea) error {
+	url := fmt.Sprintf("https://ca.jdw-apps.net/api/v0.1/jdw/venues/%d/sales-areas/%d/menus?type=available", venueID, salesArea.ID)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", "Bearer 1|SFS9MMnn5deflq0BMcUTSijwSMBB4mc7NSG2rOhqb2765466")
+
+	res, err := f.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	var menus MenusResponse
+
+	if err := json.NewDecoder(res.Body).Decode(&menus); err != nil {
+		return err
+	}
+
+	for _, menu := range menus.Data {
+		if menu.Name == "Drinks" {
+			return f.UpdateMenu(venueID, salesArea.ID, menu.ID)
+		}
+	}
+
+	// return fmt.Errorf("Menu not found :(")
+	return nil
+}
+
+func (f *Fetcher) UpdateVenue(venueID int) error {
+	url := fmt.Sprintf("https://ca.jdw-apps.net/api/v0.1/venues/%d", venueID)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", "Bearer 1|SFS9MMnn5deflq0BMcUTSijwSMBB4mc7NSG2rOhqb2765466")
+
+	res, err := f.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	var venueInfo VenueInfoResponse
+
+	if err := json.NewDecoder(res.Body).Decode(&venueInfo); err != nil {
+		return err
+	}
+
+	for _, salesArea := range venueInfo.Data.SalesAreas {
+		if err := f.UpdateBars(venueID, salesArea); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (f *Fetcher) Update() error {
@@ -40,14 +186,24 @@ func (f *Fetcher) Update() error {
 	}
 
 	for _, venue := range venues.Data {
+		// if venue.Name != "The Joseph Else" && venue.Name != "The Gooseberry Bush" {
+		// 	continue
+		// }
+
 		_, err := f.db.Exec(
 			"INSERT into Pubs (PubID, PubName, Longitude, Latitude, City) VALUES ($1, $2, $3, $4, $5)",
-			venue.ID, venue.Name, venue.Address.Location.Longitude, venue.Address.Location.Latitude, venue.Address.Town,
+			venue.VenueRef, venue.Name, venue.Address.Location.Longitude, venue.Address.Location.Latitude, venue.Address.Town,
 		)
 
 		if err != nil {
 			return err
 		}
+
+		if err := f.UpdateVenue(venue.VenueRef); err != nil {
+			return err
+		}
+
+		log.Printf("Added Venue %s", venue.Name)
 	}
 
 	return nil
--- src/internal/fetcher/types.go
+++ src/internal/fetcher/types.go
@@ -20,3 +20,64 @@ type Venue struct {
 type VenuesResponse struct {
 	Data []Venue `json:"data"`
 }
+
+type SalesArea struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type VenueInfo struct {
+	SalesAreas []SalesArea `json:"salesAreas"`
+}
+
+type VenueInfoResponse struct {
+	Data VenueInfo `json:"data"`
+}
+
+type Menu struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type MenusResponse struct {
+	Data []Menu `json:"data"`
+}
+
+type Portion struct {
+	Options []struct {
+		Value struct {
+			Price struct {
+				Value float64 `json:"value"`
+			} `json:"price"`
+		} `json:"value"`
+	} `json:"options"`
+}
+
+type Item struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Options     struct {
+		Portion Portion `json:"portion"`
+		Linked  []struct {
+			Name string `json:"name"`
+		} `json:"linked"`
+	} `json:"options"`
+}
+
+type ItemGroup struct {
+	Name  string `json:"name"`
+	Items []Item `json:"items"`
+}
+
+type Category struct {
+	Name       string      `json:"name"`
+	ItemGroups []ItemGroup `json:"itemGroups"`
+}
+
+type MenuDetail struct {
+	Categories []Category `json:"categories"`
+}
+
+type MenuDetailResponse struct {
+	Data MenuDetail `json:"data"`
+}
